0ad3a88a3efac04c0f4828b7bd7153b688b94382
ilham-asgarli <ilhamasgarli19@gmail.com>
2023-05-19 22:19:09 +0300

    Initial commit

--- lib/domain/models/user_model.g.dart
+++ lib/domain/models/user_model.g.dart
@@ -10,18 +10,18 @@ UserModel _$UserModelFromJson(Map<String, dynamic> json) => UserModel(
       json['id'] as String,
       json['email'] as String,
       json['password'] as String,
-      json['userTypeId'] as int,
-      DateTime.parse(json['createdAt'] as String),
-      DateTime.parse(json['updatedAt'] as String),
-      phoneNumber: json['phoneNumber'] as String?,
+      json['user_type_id'] as int,
+      DateTime.parse(json['created_at'] as String),
+      DateTime.parse(json['updated_at'] as String),
+      phoneNumber: json['phone_number'] as String?,
     );
 
 Map<String, dynamic> _$UserModelToJson(UserModel instance) => <String, dynamic>{
       'id': instance.id,
       'email': instance.email,
       'password': instance.password,
-      'userTypeId': instance.userTypeId,
-      'phoneNumber': instance.phoneNumber,
-      'createdAt': instance.createdAt.toIso8601String(),
-      'updatedAt': instance.updatedAt.toIso8601String(),
+      'user_type_id': instance.userTypeId,
+      'phone_number': instance.phoneNumber,
+      'created_at': instance.createdAt.toIso8601String(),
+      'updated_at': instance.updatedAt.toIso8601String(),
     };
